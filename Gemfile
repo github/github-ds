9d31e1f8ba749da6e48189a317cd82b313095a97
John Nunemaker <nunemaker@gmail.com>
2017-03-15 15:23:36 -0400

    Add ar generator for the kv migration

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
 # Specify your gem's dependencies in github-data.gemspec
 gemspec
+
+gem "rails", "~> #{ENV['RAILS_VERSION'] || '5.0.2'}"
---
+++ test/generators/github/data/active_record_generator_test.rb
@@ -0,0 +1,37 @@
+require "test_helper"
+require "rails"
+require "rails/test_help"
+require "active_record"
+require "rails/generators/test_case"
+require "generators/github/data/active_record_generator"
+
+class GitHubDataActiveRecordGeneratorTest < Rails::Generators::TestCase
+  tests GitHub::Data::Generators::ActiveRecordGenerator
+  destination File.expand_path("../../../../tmp", __FILE__)
+  setup :prepare_destination
+
+  def test_generates_migration
+    run_generator
+    assert_migration "db/migrate/create_key_values_table.rb", <<-EOM
+class CreateKeyValuesTable < ActiveRecord::Migration
+  def self.up
+    create_table :key_values do |t|
+      t.string :key, :null => false
+      t.binary :value, :null => false
+      t.datetime :expires_at, :null => true
+      t.timestamps :null => false
+    end
+
+    add_index :key_values, :key, :unique => true
+    add_index :key_values, :expires_at
+
+    change_column :key_values, :id, "bigint(20) NOT NULL AUTO_INCREMENT"
+  end
+
+  def self.down
+    drop_table :key_values
+  end
+end
+EOM
+  end
+end
