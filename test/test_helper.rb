6a1ad87a090a335c1bf499f583013fae46086a07
John Nunemaker <nunemaker@gmail.com>
2017-03-16 16:33:27 -0400

    Use migration instead of structure to create table for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,23 +6,12 @@ require "timecop"
 require "minitest/autorun"
 require "mocha/mini_test"
 
-ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.establish_connection({
   adapter: "mysql2",
   database: "github_data_test",
 })
 
 ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS `key_values`")
-ActiveRecord::Base.connection.execute(<<-EOS)
-  CREATE TABLE `key_values` (
-    `id` bigint(20) NOT NULL AUTO_INCREMENT,
-    `key` varchar(255) NOT NULL,
-    `value` blob NOT NULL,
-    `created_at` datetime NOT NULL,
-    `updated_at` datetime NOT NULL,
-    `expires_at` datetime DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `index_key_values_on_key` (`key`),
-    KEY `index_key_values_on_expires_at` (`expires_at`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8
-EOS
+require "generators/github/kv/templates/migration"
+ActiveRecord::Migration.verbose = false
+CreateKeyValuesTable.up
