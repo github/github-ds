5904050f4ac8610528a04851728aa0e404260ef4
John Nunemaker <nunemaker@gmail.com>
2017-03-16 15:49:41 -0400

    Add first example for kv

---
+++ examples/example_setup.rb
@@ -0,0 +1,28 @@
+require File.expand_path("../example_setup", __FILE__)
+require "pp"
+require "pathname"
+root_path = Pathname(__FILE__).dirname.join("..").expand_path
+lib_path  = root_path.join("lib")
+$:.unshift(lib_path)
+
+require "active_record"
+
+ActiveRecord::Base.establish_connection({
+  adapter: "mysql2",
+  database: "github_data_test",
+})
+
+ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS `key_values`")
+ActiveRecord::Base.connection.execute(<<-EOS)
+  CREATE TABLE `key_values` (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `key` varchar(255) NOT NULL,
+    `value` blob NOT NULL,
+    `created_at` datetime NOT NULL,
+    `updated_at` datetime NOT NULL,
+    `expires_at` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `index_key_values_on_key` (`key`),
+    KEY `index_key_values_on_expires_at` (`expires_at`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+EOS
---
+++ examples/kv.rb
@@ -0,0 +1,45 @@
+require File.expand_path("../example_setup", __FILE__)
+require "github/data/kv"
+
+# Create new instance using ActiveRecord's default connection.
+kv = GitHub::Data::KV.new { ActiveRecord::Base.connection }
+
+# Get a key.
+pp kv.get("foo")
+#<GitHub::Data::Result:0x3fd88cd3ea9c value: nil>
+
+# Set a key.
+kv.set("foo", "bar")
+# nil
+
+# Get the key again.
+pp kv.get("foo")
+#<GitHub::Data::Result:0x3fe810d06e4c value: "bar">
+
+# Get multiple keys at once.
+pp kv.mget(["foo", "bar"])
+#<GitHub::Data::Result:0x3fccccd1b57c value: ["bar", nil]>
+
+# Check for existence of a key.
+pp kv.exists("foo")
+#<GitHub::Data::Result:0x3fd4ae55ce8c value: true>
+
+# Check for existence of key that does not exist.
+pp kv.exists("bar")
+#<GitHub::Data::Result:0x3fd4ae55c554 value: false>
+
+# Check for existence of multiple keys at once.
+pp kv.mexists(["foo", "bar"])
+#<GitHub::Data::Result:0x3ff1e98e18e8 value: [true, false]>
+
+# Set a key's value if the key does not already exist.
+pp kv.setnx("foo", "bar")
+# false
+
+# Delete a key.
+pp kv.del("bar")
+# nil
+
+# Delete multiple keys at once.
+pp kv.mdel(["foo", "bar"])
+# nil
