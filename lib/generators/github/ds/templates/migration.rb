d939c8ac0be371fc2a55d757d93f7bdb28e364f8
Sarah Aladetan <sarahkemi@github.com>
2019-12-09 12:37:25 -0800

    update migration generator to use table name from config

--- lib/generators/github/ds/templates/migration.rb
+++ lib/generators/github/ds/templates/migration.rb
@@ -1,19 +1,19 @@
 class CreateKeyValuesTable < ActiveRecord::Migration<%= migration_version %>
   def self.up
-    create_table :key_values do |t|
+    create_table <%= table_name %> do |t|
       t.string :key, :null => false
       t.binary :value, :null => false
       t.datetime :expires_at, :null => true
       t.timestamps :null => false
     end
 
-    add_index :key_values, :key, :unique => true
-    add_index :key_values, :expires_at
+    add_index  <%= table_name %>, :key, :unique => true
+    add_index  <%= table_name %>, :expires_at
 
-    change_column :key_values, :id, "bigint(20) NOT NULL AUTO_INCREMENT"
+    change_column  <%= table_name %>, :id, "bigint(20) NOT NULL AUTO_INCREMENT"
   end
 
   def self.down
-    drop_table :key_values
+    drop_table  <%= table_name %>
   end
 end
