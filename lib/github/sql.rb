8cfaed95b680b4f051712e43389b4d76664f54a4
Adam Hess <HParker@github.com>
2020-06-15 13:35:26 -0700

    Remove collect! as it is deprecated in rails 6.1
    Rails change: https://github.com/rails/rails/pull/39589

--- lib/github/sql.rb
+++ lib/github/sql.rb
@@ -213,7 +213,7 @@ module GitHub
       # Use select_all to retrieve hashes for each row instead of arrays of values.
       @models = connection.
         select_all(query, "#{klass.name} Load via #{self.class.name}").
-          collect! { |record| klass.send :instantiate, record }
+        map { |record| klass.send :instantiate, record }
 
       retrieve_found_row_count
       freeze
